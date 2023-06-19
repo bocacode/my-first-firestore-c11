c5934adb29125bc5509ce4b9d115c16d83d82e26
Todd H. Albert <todd@bocacode.com>
2023-06-19 16:57:42 -0400

    uncomment code

--- index.js
+++ index.js
@@ -15,51 +15,51 @@ const db = getFirestore();
 
 // CRUD
 
-// const shoes = {
-//   brand: 'Birkenstock',
-//   style: 'Arizona',
-//   color: 'brown',
-//   size: 46,
-//   price: 129.99
-// }
+const shoes = {
+  brand: 'Birkenstock',
+  style: 'Arizona',
+  color: 'brown',
+  size: 46,
+  price: 129.99
+}
 
 // let's add a shirt to our clothing collection // CREATE:
-// db.collection('clothing').add(shoes)
-//   .then(doc => {
-//     console.log("Clothing added: " + doc.id);
-//   })
-//   .catch(console.error);
+db.collection('clothing').add(shoes)
+  .then(doc => {
+    console.log("Clothing added: " + doc.id);
+  })
+  .catch(console.error);
 
 // now that we have some data, let's READ (get) them // READ (ALL):
-// db.collection('clothing').get()
-//   .then(collection => {
-//     const clothing = collection.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-//     console.table(clothing);
-//   })
-//   .catch(console.error);
+db.collection('clothing').get()
+  .then(collection => {
+    const clothing = collection.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+    console.table(clothing);
+  })
+  .catch(console.error);
 
 // Let's say I want to find all of the clothing items that are >= 79.99 // READ (SOME):
-// db.collection('clothing').where('price', '>=', 79.99).get()
-//   // .where('style', '==', 'shorts')
-//   .then(collection => {
-//     const clothing = collection.docs.map(doc => ({...doc.data(), id: doc.id }));
-//     console.table(clothing);
-//   })
-//   .catch(console.error);
+db.collection('clothing').where('price', '>=', 79.99).get()
+  .where('style', '==', 'shorts')
+  .then(collection => {
+    const clothing = collection.docs.map(doc => ({...doc.data(), id: doc.id }));
+    console.table(clothing);
+  })
+  .catch(console.error);
 
 // now let's get a single document by id (we'll use await, just to show) // READ (ONE):
-// const doc = await db.collection('clothing').doc('gfhU1LsaAWyAIJ1587Ru').get()
-//               .catch(console.error);
-// const clothingItem = {...doc.data(), id: doc.id };
-// console.table(clothingItem);
+const doc = await db.collection('clothing').doc('gfhU1LsaAWyAIJ1587Ru').get()
+              .catch(console.error);
+const clothingItem = {...doc.data(), id: doc.id };
+console.table(clothingItem);
 
 // let's update a single document: // UPDATE:
-// db.collection('clothing').doc('gfhU1LsaAWyAIJ1587Ru')
-//   .update({ color: 'red', rating: 4.9 })
-//   .then(doc => console.log('Updated doc. '))
-//   .catch(console.error)
+db.collection('clothing').doc('gfhU1LsaAWyAIJ1587Ru')
+  .update({ color: 'red', rating: 4.9 })
+  .then(doc => console.log('Updated doc. '))
+  .catch(console.error)
 
 // Even though we SELDOM delete, here's how: // DELETE:
-// await db.collection('clothing').doc('u43PtL6TvBWdztsl0Ct7')
-//   .delete()
-// console.log('DELETED')
+await db.collection('clothing').doc('u43PtL6TvBWdztsl0Ct7')
+  .delete()
+console.log('DELETED')
